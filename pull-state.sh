a47092941d3e0f1b653dc5f6fc2a175add1fe992
Ian Shaw <ian.shaw1991@gmail.com>
2026-01-28 19:59:32 +0000

    Add pull and push state scripts

---
+++ pull-state.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+aws s3 ls s3://go-spacetraders-state/TVRJ.token
+if [ $? -ne 0 ]
+then
+  echo "Remote state does not exist"
+  exit 0
+else
+  aws s3 cp s3://go-spacetraders-state/TVRJ.markets.json TVRJ.markets.json 
+  aws s3 cp s3://go-spacetraders-state/TVRJ.shipyards.json TVRJ.shipyards.json 
+  # careful with this one
+  aws s3 cp s3://go-spacetraders-state/TVRJ.token TVRJ.token 
+  aws s3 cp s3://go-spacetraders-state/TVRJ.trade_routes TVRJ.trade_routes 
+  aws s3 cp s3://go-spacetraders-state/TVRJ.waypoints.json TVRJ.waypoints.json
+fi
---
+++ push-state.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+ls TVRJ.token
+if [ $? -ne 0 ]
+then
+  echo "Local state does not exist"
+  exit 0
+else
+  aws s3 cp TVRJ.markets.json s3://go-spacetraders-state/TVRJ.markets.json 
+  aws s3 cp TVRJ.shipyards.json s3://go-spacetraders-state/TVRJ.shipyards.json 
+  # careful with this one
+  aws s3 cp TVRJ.token s3://go-spacetraders-state/TVRJ.token
+  aws s3 cp TVRJ.trade_routes s3://go-spacetraders-state/TVRJ.trade_routes
+  aws s3 cp TVRJ.waypoints.json s3://go-spacetraders-state/TVRJ.waypoints.json
+fi
