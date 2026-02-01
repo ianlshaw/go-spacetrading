0317f82df4546c0c8320c5c6b9fc80fd27112b80
Ian Shaw <ian.shaw1991@gmail.com>
2026-02-01 15:18:51 +0000

    Use correct aws region

--- utils.go
+++ utils.go
@@ -184,7 +184,7 @@ func EraseState(callsign string) {
 		callsign + ".waypoints.json"}
 	ctx := context.Background()
 
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-west-2"))
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("eu-west-2"))
 	if err != nil {
 		log.Fatal(err)
 	}
